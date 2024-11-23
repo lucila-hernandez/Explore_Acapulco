6d729e8bcb492ce90927bf35250c6a5be3654ba8
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-11-22 16:06:17 -0800

    explore_acapulco

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template("home.html")
+def home():
+    return render_template('home.html')
+
+@app.route('/must_visit')
+def must_visit():
+    return render_template('must_visit.html')
+
+@app.route('/dining')
+def dining():
+    return render_template('dining.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
